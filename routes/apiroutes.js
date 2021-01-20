0b6448d339646f501508b3156097cb15d41458ba
Juno Brown <amarbrown@gmail.com>
2021-01-19 19:08:28 -0500

    added read and write functionality to api routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,5 +1,41 @@
 const router = require ("express").Router()
+const fs = require("fs");
 const path = require ("path");
+const {v4:uuidv4} = require("uuid");
+
+router.get("/api/notes", function (req, res){
+    fs.readfile("../db/db.json", function (err, data){
+        if (err) throw err;
+        res.writeHead(200, {"Content-Type": "application/json"});
+    });
+});
+
+router.post("/api/notes", function (req, res) {
+    const newNote = req.body;
+    newNote.id = uuidv4();
+
+    fs.readFile("./db/dbjson", function (err, data) {
+        if (err) throw err;
+        res.writeHead(200, {"content-Type": "application/json"});
+        res.end(data);
+    
+    let parsedData = JSON.parse(data);
+    parsedData.push(newNote);
+    let stringifiedData = JSON.stringify(parsedData);
+    res.end(stringifiedData);
+
+    fs.writeFile("./db/db.json", stringifiedData, (err) => {
+        if (err) throw err;
+        console.log("Note has been written");
+        });
+    });
+});
+
+router.del
+
+// Crud
+
+
 
 
 module.exports = apiRoutes;
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,13 +1,14 @@
 const router = require ("express").Router();
 const path = require ("path");
 
-
+//notes page
 router.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
+//home page
 router.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   module.exports = htmlRoutes;
